87a98236c987a9d7e7de12c808232423b40afd90
HamizMuzaffer <hamiz.muzaffer77@gmail.com>
2024-01-02 20:58:42 +0500

    Portfolio Website

---
+++ script.js
@@ -0,0 +1,16 @@
+// function colorChange(){
+//     navbarInput = document.getElementById("font");
+//     let text = navbarInput.innerHTML;
+//     navbarInput = text.fontcolor("white")
+    
+//   }
+
+  function changeFontColor() {
+   let button = document.getElementById("");
+   let color = button.style.color;
+   if (color == "black") { 
+      button.style.color = 'white';
+   } else {
+      button.style.color = 'black';
+   }
+}
